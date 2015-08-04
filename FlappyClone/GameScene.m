e96a57ea1083341c7fa9e446ea28c1cfed6795a2
Michael Vilabrera <m.vilabrera@gmail.com>
2015-08-04 13:58:54 -0400

    bird with infinite scrolling background

--- FlappyClone/GameScene.m
+++ FlappyClone/GameScene.m
@@ -28,10 +28,15 @@
     SKTexture *groundTexture = [SKTexture textureWithImageNamed:@"Ground"];
     groundTexture.filteringMode = SKTextureFilteringNearest;
     
+    SKAction *moveGroundSprite = [SKAction moveByX:-groundTexture.size.width * 2 y:0 duration:0.02 * groundTexture.size.width * 2];
+    SKAction *resetGroundSprite = [SKAction moveByX:groundTexture.size.width * 2 y:0 duration:0];
+    SKAction *moveGroundSpritesForever = [SKAction repeatActionForever:[SKAction sequence:@[moveGroundSprite, resetGroundSprite]]];
+    
     for (int i = 0; i < 2 + self.frame.size.width / (groundTexture.size.width * 2); i++) {
         SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:groundTexture];
         [sprite setScale:2.0];
         sprite.position = CGPointMake(i * sprite.size.width, sprite.size.height / 2);
+        [sprite runAction:moveGroundSpritesForever];
         [self addChild:sprite];
     }
     
@@ -39,11 +44,16 @@
     SKTexture *skylineTexture = [SKTexture textureWithImageNamed:@"Skyline"];
     skylineTexture.filteringMode = SKTextureFilteringNearest;
     
+    SKAction *moveSkylineSprite = [SKAction moveByX:-skylineTexture.size.width * 2 y:0 duration:0.1 * skylineTexture.size.width * 2];
+    SKAction *resetSkylineSprite = [SKAction moveByX:skylineTexture.size.width * 2 y:0 duration:0];
+    SKAction *moveSkylineSpriteForever = [SKAction repeatActionForever:[SKAction sequence:@[moveSkylineSprite, resetSkylineSprite]]];
+    
     for (int i = 0; i < 2 + self.frame.size.width / (skylineTexture.size.width * 2); i++) {
         SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:skylineTexture];
         [sprite setScale:2.0];
         sprite.zPosition = -20;
         sprite.position = CGPointMake(i * sprite.size.width, sprite.size.height / 2 + groundTexture.size.height * 2);
+        [sprite runAction:moveSkylineSpriteForever];
         [self addChild:sprite];
     }
     
